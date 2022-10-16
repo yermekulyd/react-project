4deeff7c7c8b865e44f7982ec64ec79cb8958fc2
Didar <yermekulyd@gmail.com>
2022-10-16 19:53:43 +0600

    Connect express-validator

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
 
 mongoose
     .connect ('mongodb+srv://admin:wwwwww@cluster0.vss8k5t.mongodb.net/?retryWrites=true&w=majority')
@@ -11,24 +14,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
-
-    const token = jwt.sign(
-        {
-            email: req.body.email,
-            FullName: 'Yermekuly Didar',
-        },
-        'secret123',
-    );
+app.post('/auth/register', registerValidation, (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
 
     res.json({
-        success: true, 
-        token,
+        success: true,
     });
 });
 
