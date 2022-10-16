1a303d462c12e223f58f20e98eeb5952161e2195
Didar <yermekulyd@gmail.com>
2022-10-16 22:09:19 +0600

    Authorization complete

--- index.js
+++ index.js
@@ -18,6 +18,49 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if(!user) {
+            return res.status(404).json({
+                message: 'No such user',
+            });
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if(!isValidPass) {
+            return res.status(400).json({
+                message: 'Incorrect login or password',
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret123',
+            {
+                expiresIn: '30d',
+            },
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ... userData, 
+            token,
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Authorization troubles ;c',
+        });
+    }
+});
+
 app.post('/auth/register', registerValidation, async (req, res) => {
     try {
         const errors = validationResult(req);
